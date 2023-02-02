46022938d4bd893c582347cd84cf39e1a67625cb
icaro1300 <icarogabriel1300@gmail.com>
2023-02-02 17:13:55 -0300

    SOLUCAO ATUALZADA 02-02

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace JuridicoSISCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Estadotxt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(450, 161);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(263, 203);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Estadotxt
+            // 
+            this.Estadotxt.Location = new System.Drawing.Point(44, 37);
+            this.Estadotxt.Name = "Estadotxt";
+            this.Estadotxt.Size = new System.Drawing.Size(75, 23);
+            this.Estadotxt.TabIndex = 1;
+            this.Estadotxt.Text = "Estado";
+            this.Estadotxt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Estadotxt);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Estadotxt;
+    }
+}
+
